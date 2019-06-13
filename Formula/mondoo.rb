3e77201cdb5750c17cc7f6ee67fe63e45475b275
Christoph Hartmann <chris@lollyrock.com>
2019-06-13 12:36:31 +0200

    bump to version 11.3

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -1,7 +1,7 @@
 class Mondoo < Formula
   # Update these values as needed as new versions are released
-  current_version="0.5.0"
-  current_sha256="193a4c8e062ec91e3978d7f689fce8ad9777fd00c5017857ce5758b164519c9e"
+  current_version="0.11.3"
+  current_sha256="b1c40264d8f5136c9fc8f60b790992eab616bc02c33ca20a5b6b6f88b96deefb"
 
   desc "Mondoo Labs"
   homepage "https://mondoo.io"
