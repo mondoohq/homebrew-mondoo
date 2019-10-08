d0f2bb9c9763d25480aac51efd7f4f2d3bab13f2
Christoph Hartmann <chris@lollyrock.com>
2019-10-08 08:56:11 +0200

    update to mondoo 0.28.0

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -1,7 +1,7 @@
 class Mondoo < Formula
   # Update these values as needed as new versions are released
-  current_version="0.25.0"
-  current_sha256="2baf8dc6778476d827f133a80cdfcf37e492148b1e1a9e1eeb7095a1549b9ce8"
+  current_version="0.28.0"
+  current_sha256="7ca190289af681cda939b73ffd9260b4ad3f5fa2b4e1cbc900ef765f1cb18aa1"
 
   desc "Mondoo Labs"
   homepage "https://mondoo.io"
