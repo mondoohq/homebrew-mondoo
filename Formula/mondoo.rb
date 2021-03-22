d1bbd2a17b7dbb61f0e51dad1e83539a49dc7279
Christoph Hartmann <chris@lollyrock.com>
2021-03-22 23:55:54 +0100

    bump to mondoo 3.7.2

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -1,7 +1,7 @@
 class Mondoo < Formula
   # Update these values as needed as new versions are released
-  current_version="3.7.0"
-  current_sha256="09a986f1ce1cbb66dcd87b8f7e164b5825c244358e641eacd6d9250c998b6f52"
+  current_version="3.7.2"
+  current_sha256="99ef8767c08a07d46d58bd78f60144b62800df3da32fc9f9e3be8737efc05cd2"
 
   desc "Mondoo Labs"
   homepage "https://mondoo.io"
