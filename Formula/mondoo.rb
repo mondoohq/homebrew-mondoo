8a030ce414970a959c37e1ace01115504aeb53bc
Jay Mundrawala <jay@thechamberofunderstanding.com>
2021-08-24 12:52:50 -0500

    4.17.0

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -1,13 +1,17 @@
 class Mondoo < Formula
   # Update these values as needed as new versions are released
-  current_version="4.16.0"
-  current_sha256="47f1176135f9970731b3d7cfc8cd0925539eceb4e4fe4fc7326e8630bb242367"
+  current_version="4.17.0"
+  if Hardware::CPU.intel?
+    sha256 "1e280d332ca8518e4ca00238a440e5a5ea9c016f9948bafb526a29630d3c8cdb"
+    url "https://releases.mondoo.io/mondoo/#{current_version}/mondoo_#{current_version}_darwin_amd64.tar.gz"
+  else
+    sha256 "51b9bb14fb72ef3cb01ce4ea4ef0751e4b1e8f487db8828e38ed44f603ff52a2"
+    url "https://releases.mondoo.io/mondoo/#{current_version}/mondoo_#{current_version}_darwin_arm64.tar.gz"
+  end
 
   desc "Mondoo Labs"
   homepage "https://mondoo.io"
-  url "https://releases.mondoo.io/mondoo/#{current_version}/mondoo_#{current_version}_darwin_amd64.tar.gz"
   version current_version
-  sha256 current_sha256.downcase
 
   def install
     bin.install "mondoo"
