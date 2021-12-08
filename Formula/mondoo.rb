f9c7760a8fc71882756006369e8eb9dcfe3fe331
Victoria Jeffrey <vj@email.com>
2021-12-07 17:27:56 -0700

    5.17.1

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -2,14 +2,14 @@
 class Mondoo < Formula
   desc "Mondoo"
   homepage "https://mondoo.io"
-  version "5.17.0"
+  version "5.17.1"
 
   if Hardware::CPU.intel?
-    sha256 "3984c4edb0346ac88884753623ba9530d52f3701166e3747cb44f1ddabb04b57"
-    url "https://releases.mondoo.io/mondoo/5.17.0/mondoo_5.17.0_darwin_amd64.tar.gz"
+    sha256 "d5071071cf5ef387d6e1d898abcabf983ce246ec5b4cd7d8f1f97c2956b8b9f7"
+    url "https://releases.mondoo.io/mondoo/5.17.1/mondoo_5.17.1_darwin_amd64.tar.gz"
   else
-    sha256 "74f5470c41f4c36cef07bd2d0bc7024a4f6b3fc83835db09b4285c98612006b3"
-    url "https://releases.mondoo.io/mondoo/5.17.0/mondoo_5.17.0_darwin_arm64.tar.gz"
+    sha256 "a0e032d42924ae59e56d45e5498fdea2af328be633e29578fc2dc6b86402fbdd"
+    url "https://releases.mondoo.io/mondoo/5.17.1/mondoo_5.17.1_darwin_arm64.tar.gz"
   end
 
   def install
