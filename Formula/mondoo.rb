6213fee8ea74756d44d2ee8ca3637518f096a109
Christoph Hartmann <chris@lollyrock.com>
2021-12-22 12:11:46 +0100

    5.19.1

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -2,14 +2,14 @@
 class Mondoo < Formula
   desc "Mondoo"
   homepage "https://mondoo.io"
-  version "5.19.0"
+  version "5.19.1"
 
   if Hardware::CPU.intel?
-    sha256 "8ad055cb634de5241a855d89f01d6712d2db426c76a0769e5355e66599fd6cee"
-    url "https://releases.mondoo.io/mondoo/5.19.0/mondoo_5.19.0_darwin_amd64.tar.gz"
+    sha256 "afdbbe936e5b706302124a1925e7cfc19e1bb80b8c9041225ca4586c1f8facf2"
+    url "https://releases.mondoo.io/mondoo/5.19.1/mondoo_5.19.1_darwin_amd64.tar.gz"
   else
-    sha256 "2e1a662eef73f4e838a5b44b89f19f3d48826f1d067f519d48ff5e96dcb1c5d9"
-    url "https://releases.mondoo.io/mondoo/5.19.0/mondoo_5.19.0_darwin_arm64.tar.gz"
+    sha256 "76225b37deb22d1cc2615fb1c7da7f7085da46ad4a26df96a5d2de2eeee4e3c0"
+    url "https://releases.mondoo.io/mondoo/5.19.1/mondoo_5.19.1_darwin_arm64.tar.gz"
   end
 
   def install
