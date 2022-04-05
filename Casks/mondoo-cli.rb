4efc576bcc4a28152b50ef2e5a5fc0e288ad77a3
Jay Mundrawala <jay@thechamberofunderstanding.com>
2022-04-05 15:11:11 -0500

    5.33.0

--- Casks/mondoo-cli.rb
+++ Casks/mondoo-cli.rb
@@ -2,11 +2,11 @@
 cask "mondoo-cli" do
   name "Mondoo"
   desc "Mondoo Client CLI for the Mondoo Policy as Code Platform"
-  version "5.32.0"
+  version "5.33.0"
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "5cf6aa1485c346e9417995333cea2f6073f294d417fd0e9d4bc7af352f5d927f"
+  sha256 "d82ff0cc2274dd43ef16c5ae873186934399d875c9168ed895c0179aa7780f22"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -2,14 +2,14 @@
 class Mondoo < Formula
   desc "Mondoo Client CLI for the Mondoo Policy as Code Platform"
   homepage "https://mondoo.com"
-  version "5.32.0"
+  version "5.33.0"
 
   if Hardware::CPU.intel?
-    sha256 "a377e3445d2a424880218f4cae710b5c0850598f3c52de744d7f1a189336276b"
-    url "https://releases.mondoo.com/mondoo/5.32.0/mondoo_5.32.0_darwin_amd64.tar.gz"
+    sha256 "1d7ae4b27bc352059f38782d0c56cbe4194259fb6d0a59c49486988309e3d660"
+    url "https://releases.mondoo.com/mondoo/5.33.0/mondoo_5.33.0_darwin_amd64.tar.gz"
   else
-    sha256 "fe0f207c641ba53ea27d76a923d19a80d35bdd8a3c099f1289a83013fd4c220f"
-    url "https://releases.mondoo.com/mondoo/5.32.0/mondoo_5.32.0_darwin_arm64.tar.gz"
+    sha256 "dcc65ab2f89546819ad436bc824239224142e101f0bf87a9d159a120d78ba8a5"
+    url "https://releases.mondoo.com/mondoo/5.33.0/mondoo_5.33.0_darwin_arm64.tar.gz"
   end
 
   def install
