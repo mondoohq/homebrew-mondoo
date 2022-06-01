053e14f6a23a955b5b06d9aa068320a7c265fc43
Jay Mundrawala <jay@thechamberofunderstanding.com>
2022-06-01 11:52:45 -0500

    6.1.0

--- Casks/mondoo-cli.rb
+++ Casks/mondoo-cli.rb
@@ -2,11 +2,11 @@
 cask "mondoo-cli" do
   name "Mondoo"
   desc "Mondoo Client CLI for the Mondoo Policy as Code Platform"
-  version "6.0.1"
+  version "6.1.0"
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "2bd03aedb6597cc497e8df85d768514dd9485630095f2719af8fe00e5c1048a5"
+  sha256 "aaf0ae9f4b42d95a11b269ccb3a7034cffa0fc062c36060839a11cb4cf863f6d"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
