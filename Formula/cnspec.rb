3e9aa0eccc51a625a70808f16b4ede23deaf9309
Christoph Hartmann <chris@lollyrock.com>
2022-10-04 01:29:44 +0200

    ⭐️ update to cnspec 6.18.0-alpha3 (#13)

--- Formula/cnspec.rb
+++ Formula/cnspec.rb
@@ -2,14 +2,14 @@
 class Cnspec < Formula
   desc "Cloud-Native Security and Policy Framework "
   homepage "https://mondoo.com"
-  version "6.18.0-alpha2"
+  version "6.18.0-alpha3"
 
   if Hardware::CPU.intel?
-    sha256 "e961356daeaf3a8075a5e56cbe23599f46abed27311005ed695c9cdf709b5374"
-    url "https://releases.mondoo.com/cnspec/6.18.0-alpha2/cnspec_6.18.0-alpha2_darwin_amd64.tar.gz"
+    sha256 "3d8c20f5d0d8322bf9bca3a9a98d1cf81ad2ca5bc659641c5e702f1a1000b17b"
+    url "https://releases.mondoo.com/cnspec/6.18.0-alpha3/cnspec_6.18.0-alpha3_darwin_amd64.tar.gz"
   else
-    sha256 "ad9db1c31ab80d9e0973a1481e0e8f243322aac32a0a938162321edce62d359e"
-    url "https://releases.mondoo.com/cnspec/6.18.0-alpha2/cnspec_6.18.0-alpha2_darwin_arm64.tar.gz"
+    sha256 "6b6294b80b03288a64899d0a083ada0259a0b72fac6a9c7c8227ca5e3d5df7f9"
+    url "https://releases.mondoo.com/cnspec/6.18.0-alpha3/cnspec_6.18.0-alpha3_darwin_arm64.tar.gz"
   end
 
   def install
