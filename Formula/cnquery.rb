0e5f2a2af78e0817f995372a3862a87f1675a613
Mondoo Tools <tools@mondoo.com>
2022-11-01 14:24:25 +0000

    7.2.1

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -2,14 +2,14 @@
 class Cnquery < Formula
   desc "Cloud-Native Query - Asset Inventory Framework"
   homepage "https://mondoo.com"
-  version "7.2.0"
+  version "7.2.1"
 
   if Hardware::CPU.intel?
-    sha256 "21e08c7471be2cfd17f09016572839021f58655e6cba6900e2dcf0f02b00fb8f"
-    url "https://releases.mondoo.com/cnquery/7.2.0/cnquery_7.2.0_darwin_amd64.tar.gz"
+    sha256 "f1b600dc30987554b222384de7940a78ef41a3ff147762705d804bc9e041ec35"
+    url "https://releases.mondoo.com/cnquery/7.2.1/cnquery_7.2.1_darwin_amd64.tar.gz"
   else
-    sha256 "bd01ccff7472371949851a6065d656269737d5ccac405e314c47550f5588032d"
-    url "https://releases.mondoo.com/cnquery/7.2.0/cnquery_7.2.0_darwin_arm64.tar.gz"
+    sha256 "814f23dd360c6015841a44e4f8af1171fcc93442bb855ffebfabae8f3bd18871"
+    url "https://releases.mondoo.com/cnquery/7.2.1/cnquery_7.2.1_darwin_arm64.tar.gz"
   end
 
   def install
