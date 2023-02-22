69cb3851d5f70b5e54fb8a0c494206e1dd89ba0a
Mondoo Tools <tools@mondoo.com>
2023-02-22 12:41:15 +0000

    7.18.1

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -2,14 +2,14 @@
 class Mondoo < Formula
   desc "Mondoo Client CLI for the Mondoo Policy as Code Platform"
   homepage "https://mondoo.com"
-  version "7.18.0"
+  version "7.18.1"
 
   if Hardware::CPU.intel?
-    sha256 "7251a821d6911fd3a0240207c0553d40e90952bbd1146d292ee7f82e4f6106e5"
-    url "https://releases.mondoo.com/mondoo/7.18.0/mondoo_7.18.0_darwin_amd64.tar.gz"
+    sha256 "2325d837117f9597a72b429e60fe128f669a6c4a936593e69f764063a942dd33"
+    url "https://releases.mondoo.com/mondoo/7.18.1/mondoo_7.18.1_darwin_amd64.tar.gz"
   else
-    sha256 "0e4ae1e74ddb3f4d630019b1b3214b49348bde4a684fc58b552c5edc4b8f5c52"
-    url "https://releases.mondoo.com/mondoo/7.18.0/mondoo_7.18.0_darwin_arm64.tar.gz"
+    sha256 "db860d61d917d17794b53f72a3f0fe04a5601143cd579adcb3186feee9ce4a16"
+    url "https://releases.mondoo.com/mondoo/7.18.1/mondoo_7.18.1_darwin_arm64.tar.gz"
   end
 
   def install
