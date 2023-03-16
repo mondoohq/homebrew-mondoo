6f9fa60c07eefc1a8560c484a70a3e451ba23641
Ben Rockwood <benr@cuddletech.com>
2023-03-16 16:47:04 -0700

    Mondoo Metapackage
    Update package to install cnquery and cnspec as dependancies

--- generator/formula.go
+++ generator/formula.go
@@ -52,6 +52,8 @@ class {{ .Class }} < Formula
   desc "{{ .Desc }}"
   homepage "{{ .Homepage }}"
   version "{{ .Version }}"
+  depends_on "cnquery"
+  depends_on "cnspec"
 
   if Hardware::CPU.intel?
     sha256 "{{ .Sha256Amd64 }}"
