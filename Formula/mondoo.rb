d86cb3205f0b92d6edb94065097d45a912341107
Ben Rockwood <benr@cuddletech.com>
2023-03-28 12:47:06 -0700

    Update mondoo.rb

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -6,10 +6,10 @@ class Mondoo < Formula
   depends_on "cnspec"
 
   if Hardware::CPU.intel?
-    sha256 "7436f48959f49e07573a3ad8f4a15d85558a00993d093c22b06c2f7327ff7762"
+    sha256 "482319f9e3a315e5cdf5c0fbb2b7e18dab8e2d177619b378d5db00edab77ab91"
     url "https://releases.mondoo.com/mondoo/8.3.0/mondoo_8.3.0_darwin_amd64.tar.gz"
   else
-    sha256 "db164649c1b705de33b7957e94bf883ebdb9eaff8641fe0088750acf87249013"
+    sha256 "482319f9e3a315e5cdf5c0fbb2b7e18dab8e2d177619b378d5db00edab77ab91"
     url "https://releases.mondoo.com/mondoo/8.3.0/mondoo_8.3.0_darwin_arm64.tar.gz"
   end
 
