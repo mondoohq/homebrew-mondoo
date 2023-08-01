de878e4a3a43d6def8797a97b0df9b09fb37e71a
Mondoo Tools <tools@mondoo.com>
2023-08-01 11:11:11 +0000

    8.21.0

--- Casks/mondoo-cli.rb
+++ Casks/mondoo-cli.rb
@@ -6,7 +6,7 @@ cask "mondoo-cli" do
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "70279fcbfa83ab47c2dbb9f28d57cba4b97594b03acdb0899e2877eefacc5e89"
+  sha256 "762b0b50c19a9f6ee500bd9edd03ee5c4fb9c1ced71a40a2939ecb9a55b59069"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -6,10 +6,10 @@ class Mondoo < Formula
   depends_on "cnspec"
 
   if Hardware::CPU.intel?
-    sha256 "24c22aef3f4a8a68d54cf730fed988def670161ab775e53f72307fceb58fa5e1"
+    sha256 "5252b3429a8874075212cfff25cfe3154c5605edf163668926ad3337e2574356"
     url "https://releases.mondoo.com/mondoo/8.21.0/mondoo_8.21.0_darwin_amd64.tar.gz"
   else
-    sha256 "24c22aef3f4a8a68d54cf730fed988def670161ab775e53f72307fceb58fa5e1"
+    sha256 "5252b3429a8874075212cfff25cfe3154c5605edf163668926ad3337e2574356"
     url "https://releases.mondoo.com/mondoo/8.21.0/mondoo_8.21.0_darwin_arm64.tar.gz"
   end
 
