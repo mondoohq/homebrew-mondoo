d2ea562ac920a040484be581268cd7ce4ba9251e
Ben Rockwood <benr@cuddletech.com>
2023-10-05 13:34:43 -0700

    Update mondoo.rb

--- Formula/mondoo.rb
+++ Formula/mondoo.rb
@@ -4,21 +4,5 @@ class Mondoo < Formula
   homepage "https://mondoo.com"
   version "9.0.0"
   depends_on "cnspec"
-
-  if Hardware::CPU.intel?
-    sha256 ""
-    url "https://releases.mondoo.com/mondoo/9.0.0/mondoo_9.0.0_darwin_amd64.tar.gz"
-  else
-    sha256 ""
-    url "https://releases.mondoo.com/mondoo/9.0.0/mondoo_9.0.0_darwin_arm64.tar.gz"
-  end
-
-  def install
-    bin.install "mondoo"
-  end
-
-  test do
-    system "#{bin}/mondoo --version"
-  end
 end
 
