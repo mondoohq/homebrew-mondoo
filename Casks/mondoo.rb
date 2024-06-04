b99341a71274228e65d49a21891bfc51abed41fe
Mondoo Tools <tools@mondoo.com>
2024-06-04 17:28:42 +0000

    11.7.0

--- Casks/mondoo.rb
+++ Casks/mondoo.rb
@@ -9,7 +9,7 @@ cask "mondoo" do
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "93ad1e4a919de8b94ae5c1ee9a81031ae76dc80c5d88e71389951e3af0e1e64b"
+  sha256 "2f807e73f7122b2ae530ddb43d33cb2f702dbd0f27414c82de286fa048c2269c"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
