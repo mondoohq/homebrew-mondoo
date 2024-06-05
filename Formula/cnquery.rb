73a3496a05668bb0ff97dbb34f2905c3c5ca6735
Mondoo Tools <tools@mondoo.com>
2024-06-05 15:52:12 +0000

    11.7.2

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -5,15 +5,15 @@
 class Cnquery < Formula
   desc "Cloud-Native Query - Asset Inventory Framework"
   homepage "https://mondoo.com"
-  version "11.7.1"
+  version "11.7.2"
   
 
   if Hardware::CPU.intel?
-    sha256 "4116c8640d16238f89415a59d20b97187d71b9d042617babb9377f284e6e3225"
-    url "https://releases.mondoo.com/cnquery/11.7.1/cnquery_11.7.1_darwin_amd64.tar.gz"
+    sha256 "dd962b564f05a985dc5237af20fa2ca4199af0b6245e266af0e96bc6adf175dd"
+    url "https://releases.mondoo.com/cnquery/11.7.2/cnquery_11.7.2_darwin_amd64.tar.gz"
   else
-    sha256 "847a34ff9d69de5b4202bf13d2839849749a6e894a1e27a3fedca96933a3e191"
-    url "https://releases.mondoo.com/cnquery/11.7.1/cnquery_11.7.1_darwin_arm64.tar.gz"
+    sha256 "33dbd90a90196b5181c83d914795b89e88a8eaaa632e68510545f47878913292"
+    url "https://releases.mondoo.com/cnquery/11.7.2/cnquery_11.7.2_darwin_arm64.tar.gz"
   end
 
   def install
--- Formula/cnspec.rb
+++ Formula/cnspec.rb
@@ -5,15 +5,15 @@
 class Cnspec < Formula
   desc "Cloud-Native Security and Policy Framework "
   homepage "https://mondoo.com"
-  version "11.7.1"
+  version "11.7.2"
   depends_on "cnquery"
 
   if Hardware::CPU.intel?
-    sha256 "009a7a436448176d3fff5f130861710f30ec6b599d803702b474201ffc6ae860"
-    url "https://releases.mondoo.com/cnspec/11.7.1/cnspec_11.7.1_darwin_amd64.tar.gz"
+    sha256 "97d1cb5347958a6631120581058b7074d9768f9bed079d4355622410ed73c0f0"
+    url "https://releases.mondoo.com/cnspec/11.7.2/cnspec_11.7.2_darwin_amd64.tar.gz"
   else
-    sha256 "a5e28f643d759c9802c014719ae723eaff7df95da2dd60014b2cfc81ff308312"
-    url "https://releases.mondoo.com/cnspec/11.7.1/cnspec_11.7.1_darwin_arm64.tar.gz"
+    sha256 "342d6f2e7eccfeb093c5e89faee36514a3935b685b6f35c4a3b745fa6ecff2f1"
+    url "https://releases.mondoo.com/cnspec/11.7.2/cnspec_11.7.2_darwin_arm64.tar.gz"
   end
 
   def install
