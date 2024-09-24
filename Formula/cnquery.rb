3e9d2d568c6ea42193d2d0b750c7eb21932bc7f7
Mondoo Tools <tools@mondoo.com>
2024-09-24 12:52:10 +0000

    11.23.0

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -5,15 +5,15 @@
 class Cnquery < Formula
   desc "Cloud-Native Query - Asset Inventory Framework"
   homepage "https://mondoo.com"
-  version "11.22.0"
+  version "11.23.0"
   
 
   if Hardware::CPU.intel?
-    sha256 "05125ed66e0a7365bf938ad6b9d34850c75bd4950559ed258e06c008e7648fd3"
-    url "https://releases.mondoo.com/cnquery/11.22.0/cnquery_11.22.0_darwin_amd64.tar.gz"
+    sha256 "07c7a001a1b6198f7fe184db082faa4dcc9556cee2eee1961cc6c1ae90c815c8"
+    url "https://releases.mondoo.com/cnquery/11.23.0/cnquery_11.23.0_darwin_amd64.tar.gz"
   else
-    sha256 "e2f9cdf00eccf4f388977ab615709b91fa4ad7f9a71ab135916fef6aa57b9344"
-    url "https://releases.mondoo.com/cnquery/11.22.0/cnquery_11.22.0_darwin_arm64.tar.gz"
+    sha256 "e7b8f5adb3c6bde1174ff1ab4e516f3c77d8b23cdc0fbf42fea9e80d3c5c096c"
+    url "https://releases.mondoo.com/cnquery/11.23.0/cnquery_11.23.0_darwin_arm64.tar.gz"
   end
 
   def install
--- Formula/cnspec.rb
+++ Formula/cnspec.rb
@@ -5,15 +5,15 @@
 class Cnspec < Formula
   desc "Cloud-Native Security and Policy Framework "
   homepage "https://mondoo.com"
-  version "11.22.0"
+  version "11.23.0"
   depends_on "cnquery"
 
   if Hardware::CPU.intel?
-    sha256 "8a7fa750101424e34373218d6397f8d6fe83b47c2672c41c816811abc6ddd4cd"
-    url "https://releases.mondoo.com/cnspec/11.22.0/cnspec_11.22.0_darwin_amd64.tar.gz"
+    sha256 "d7acff134c5f2ea09b2dc429f60b7c3432743dc4a5f31c0e01ef7fa3feec3bd6"
+    url "https://releases.mondoo.com/cnspec/11.23.0/cnspec_11.23.0_darwin_amd64.tar.gz"
   else
-    sha256 "ecb9ec92042e2cd883c9726a2d51a764ccc617b1068d3c31966299d508c9374a"
-    url "https://releases.mondoo.com/cnspec/11.22.0/cnspec_11.22.0_darwin_arm64.tar.gz"
+    sha256 "d66ba85385af7f363daa1b1e556473f947a8be04db3e3e51a66721289296b6a1"
+    url "https://releases.mondoo.com/cnspec/11.23.0/cnspec_11.23.0_darwin_arm64.tar.gz"
   end
 
   def install
