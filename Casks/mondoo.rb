56778531fba74f50af19ba32968bc9306e4d490e
Mondoo Tools <tools@mondoo.com>
2025-01-14 12:30:57 +0000

    11.37.0

--- Casks/mondoo.rb
+++ Casks/mondoo.rb
@@ -5,11 +5,11 @@
 cask "mondoo" do
   name "Mondoo"
   desc "Mondoo Enterprise Package for cnquery and cnspec"
-  version "11.36.2"
+  version "11.37.0"
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "e05aedf96b6cbf8e4b1ecfd4258401399c99cfba7a33101ef21c3ce3eef4d865"
+  sha256 "fadb0749fb04eba1ebb8f76bd916308fd09395ec056254aeb821cb93dc1864bb"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
