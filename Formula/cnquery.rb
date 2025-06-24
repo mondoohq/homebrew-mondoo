c702fb342298165f830fdc68e98102fc9e20c7c9
Mondoo Tools <tools@mondoo.com>
2025-06-24 09:35:07 +0000

    11.60.0

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -9,10 +9,10 @@ class Cnquery < Formula
   
 
   if Hardware::CPU.intel?
-    sha256 ""
+    sha256 "53ae0b90f9fc7e5fb72231f1b75ecc484188fd653634280aa93730425ff685f8"
     url "https://releases.mondoo.com/cnquery/11.60.0/cnquery_11.60.0_darwin_amd64.tar.gz"
   else
-    sha256 ""
+    sha256 "cc4817784dbffd2f0ad8efd73ef9947645c8046406c419117de59ce69a06ff6e"
     url "https://releases.mondoo.com/cnquery/11.60.0/cnquery_11.60.0_darwin_arm64.tar.gz"
   end
 
