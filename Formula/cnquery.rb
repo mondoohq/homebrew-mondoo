4f1086c29aca293d13d788dd3e6a405312fb2e99
Christian Zunker <827818+czunker@users.noreply.github.com>
2026-04-28 13:25:35 +0200

    🧹 Bump to v13.7.0 (#64)
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -5,15 +5,15 @@
 class Cnquery < Formula
   desc "Transitional package for cnquery to cnspec migration"
   homepage "https://mondoo.com"
-  version "13.6.0"
+  version "13.7.0"
   depends_on "cnspec"
 
   if Hardware::CPU.intel?
-    sha256 "9a07a773a97f29ac8a2f16718299a86ecc3c2837775b5366164218618c33d37a"
-    url "https://releases.mondoo.com/cnspec/13.6.0/cnspec_13.6.0_darwin_amd64.tar.gz"
+    sha256 "2a58b80095ba1cacc3337a89c12f75ad2cb099eebade3415339974b9c5592478"
+    url "https://releases.mondoo.com/cnspec/13.7.0/cnspec_13.7.0_darwin_amd64.tar.gz"
   else
-    sha256 "d29c6a5c042ed6105b72097b934d13287f9bf22ce5eaf0d144f4750915aa9a8d"
-    url "https://releases.mondoo.com/cnspec/13.6.0/cnspec_13.6.0_darwin_arm64.tar.gz"
+    sha256 "6971416c488f0f0212d4bb468b5d16c1b1cabbb6e2170133864d3b87511ad32f"
+    url "https://releases.mondoo.com/cnspec/13.7.0/cnspec_13.7.0_darwin_arm64.tar.gz"
   end
 
   def install
--- Formula/mql.rb
+++ Formula/mql.rb
@@ -5,15 +5,15 @@
 class Mql < Formula
   desc "MQL - Asset Inventory Query Language"
   homepage "https://mondoo.com"
-  version "13.6.0"
+  version "13.7.0"
   
 
   if Hardware::CPU.intel?
-    sha256 "970cfa4a914a9ca4d37b3689b6704848afce8ef7d1f9c3fc1e1b74285be60074"
-    url "https://releases.mondoo.com/mql/13.6.0/mql_13.6.0_darwin_amd64.tar.gz"
+    sha256 "d011dc077027e59e71ad737fd1af531b15d20ca74070164e0df6feae641b20e4"
+    url "https://releases.mondoo.com/mql/13.7.0/mql_13.7.0_darwin_amd64.tar.gz"
   else
-    sha256 "c3da4edc22e6e0eb3618cd1015b62cc0b691b2f14aec60d8e45bd931862b2f1c"
-    url "https://releases.mondoo.com/mql/13.6.0/mql_13.6.0_darwin_arm64.tar.gz"
+    sha256 "2fbfbc4efba3c359b93ab48a51b24598d840472059dc0b572361bdc36bbb9cfa"
+    url "https://releases.mondoo.com/mql/13.7.0/mql_13.7.0_darwin_arm64.tar.gz"
   end
 
   def install
