4292cb5d87514e441cee99cac25e731427e851fe
Mondoo Tools <tools@mondoo.com>
2026-05-19 09:03:42 +0000

    13.10.0

--- Formula/cnspec.rb
+++ Formula/cnspec.rb
@@ -5,15 +5,15 @@
 class Cnspec < Formula
   desc "Cloud-Native Security and Policy Framework"
   homepage "https://mondoo.com"
-  version "13.9.0"
+  version "13.10.0"
   depends_on "mql"
 
   if Hardware::CPU.intel?
-    sha256 "35b859ce88b241a1514a2ec6b48e38861ab8dda1db2fd52291e6a845ea3644ed"
-    url "https://releases.mondoo.com/cnspec/13.9.0/cnspec_13.9.0_darwin_amd64.tar.gz"
+    sha256 "9f2ac20635d628eab00d510c9f5032b4a2fc4f2dc57ed4a615fb0c0273d81571"
+    url "https://releases.mondoo.com/cnspec/13.10.0/cnspec_13.10.0_darwin_amd64.tar.gz"
   else
-    sha256 "7370eff7eba53007dcacf375867331b338ec24603c45dea30488d09986f8eaa0"
-    url "https://releases.mondoo.com/cnspec/13.9.0/cnspec_13.9.0_darwin_arm64.tar.gz"
+    sha256 "8cfe8e8dcad6cb5e39db49df94c48399dd67e37dd9fd52b5b222a4a75bfaaf27"
+    url "https://releases.mondoo.com/cnspec/13.10.0/cnspec_13.10.0_darwin_arm64.tar.gz"
   end
 
   def install
