09089d66142552655261ccf09e7357b2e167bc61
Mondoo Tools <tools@mondoo.com>
2026-07-01 22:29:58 +0000

    13.27.1

--- Casks/mondoo-cli.rb
+++ Casks/mondoo-cli.rb
@@ -5,11 +5,11 @@
 cask "mondoo-cli" do
   name "Mondoo"
   desc "Mondoo Enterprise Package for cnquery and cnspec"
-  version "13.27.0"
+  version "13.27.1"
   homepage "https://mondoo.com"
 
   url "https://releases.mondoo.com/mondoo/#{version}/mondoo_#{version}_darwin_universal.pkg"
-  sha256 "d37f65c887972cf56d540134eea6c774af3cf33c9daa2c292d473c4cdf25010b"
+  sha256 "d477523d5cefd3dec9a05ab2f90c743ab9f2eb4cfec3d031dd4733ab0aac66a9"
 
   livecheck do
     url "https://releases.mondoo.com/mondoo/latest/index.html"
--- Formula/mql.rb
+++ Formula/mql.rb
@@ -5,15 +5,15 @@
 class Mql < Formula
   desc "MQL - Asset Inventory Query Language"
   homepage "https://mondoo.com"
-  version "13.27.0"
+  version "13.27.1"
   
 
   if Hardware::CPU.intel?
-    sha256 "ca2e1094feac393770d713a0c6d3f224f5d0c0fc641d6462aee162d84fed1997"
-    url "https://releases.mondoo.com/mql/13.27.0/mql_13.27.0_darwin_amd64.tar.gz"
+    sha256 "83abe0608aeb8d252877e0248ac96cb426bb8552ec6544c97dd01b1fc1d26d9d"
+    url "https://releases.mondoo.com/mql/13.27.1/mql_13.27.1_darwin_amd64.tar.gz"
   else
-    sha256 "c7e62253eb2786ab813180e0d9b47d8f72829d169ab3c03062a6a12161ffd1a6"
-    url "https://releases.mondoo.com/mql/13.27.0/mql_13.27.0_darwin_arm64.tar.gz"
+    sha256 "bb696e45013f34891aa9899c1fa1ce069fa95c6b49e23bbf66648865570eb9e3"
+    url "https://releases.mondoo.com/mql/13.27.1/mql_13.27.1_darwin_arm64.tar.gz"
   end
 
   def install
