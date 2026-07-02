13204e5c71cd1239576a0926b3519a9e2c466ef8
Mondoo Tools <tools@mondoo.com>
2026-07-02 16:21:24 +0000

    13.27.4

--- Formula/mql.rb
+++ Formula/mql.rb
@@ -5,15 +5,15 @@
 class Mql < Formula
   desc "MQL - Asset Inventory Query Language"
   homepage "https://mondoo.com"
-  version "13.27.3"
+  version "13.27.4"
   
 
   if Hardware::CPU.intel?
-    sha256 "684058183c5fcb147bc7f09d6135631375f27e8229edd5d9dca2b143ac799bc2"
-    url "https://releases.mondoo.com/mql/13.27.3/mql_13.27.3_darwin_amd64.tar.gz"
+    sha256 "d51699147f8b11f2e3a93112641e549d0440f5b364d0ce4e3aae32450c5149fe"
+    url "https://releases.mondoo.com/mql/13.27.4/mql_13.27.4_darwin_amd64.tar.gz"
   else
-    sha256 "39f6723827870b28a95907fde5f7d9510ecb8511d97bd1bf6a875555d4b4f4a1"
-    url "https://releases.mondoo.com/mql/13.27.3/mql_13.27.3_darwin_arm64.tar.gz"
+    sha256 "262cdacddab7097471b13a759a665cef9e9171fe51b8dce85fca52347eda28e3"
+    url "https://releases.mondoo.com/mql/13.27.4/mql_13.27.4_darwin_arm64.tar.gz"
   end
 
   def install
