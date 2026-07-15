e5df9b7e965596672c556719d4d15a8909f5328a
Mondoo Tools <tools@mondoo.com>
2026-07-15 20:40:31 +0000

    13.29.1

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -5,15 +5,15 @@
 class Cnquery < Formula
   desc "Transitional package for cnquery to cnspec migration"
   homepage "https://mondoo.com"
-  version "13.29.0"
+  version "13.29.1"
   depends_on "cnspec"
 
   if Hardware::CPU.intel?
-    sha256 "2a553cb2f0936f207d1e29c389087ddba5bf157aff5d1d8aa80ce4822246e21d"
-    url "https://releases.mondoo.com/cnspec/13.29.0/cnspec_13.29.0_darwin_amd64.tar.gz"
+    sha256 "b92b1c27cbf3c5d90e8e684dd627e78875211f110d5f2e50925b458da4f89c1d"
+    url "https://releases.mondoo.com/cnspec/13.29.1/cnspec_13.29.1_darwin_amd64.tar.gz"
   else
-    sha256 "73293c5fa3a4ac7fc2aadb2c4dfaf1a096cc7634a662d24a7be66e32590fe9dd"
-    url "https://releases.mondoo.com/cnspec/13.29.0/cnspec_13.29.0_darwin_arm64.tar.gz"
+    sha256 "0ba1cef4db3a5bc88ec31a680bf36caae88d11bf72349cca77a0cd1761912b34"
+    url "https://releases.mondoo.com/cnspec/13.29.1/cnspec_13.29.1_darwin_arm64.tar.gz"
   end
 
   def install
--- Formula/mql.rb
+++ Formula/mql.rb
@@ -5,15 +5,15 @@
 class Mql < Formula
   desc "MQL - Asset Inventory Query Language"
   homepage "https://mondoo.com"
-  version "13.29.0"
+  version "13.29.1"
   
 
   if Hardware::CPU.intel?
-    sha256 "c12f34d2b72b5310fbe83393c3297008e5c05a950bf1210bd53b8ba2963b0197"
-    url "https://releases.mondoo.com/mql/13.29.0/mql_13.29.0_darwin_amd64.tar.gz"
+    sha256 "a99e4f05607be3b6f19119ad857c4c75fea33295449bd550560ceaad96f51db6"
+    url "https://releases.mondoo.com/mql/13.29.1/mql_13.29.1_darwin_amd64.tar.gz"
   else
-    sha256 "89c0fa0111d4fe691b32817defaaf205512005503a0cf846dd54132ec8e843b1"
-    url "https://releases.mondoo.com/mql/13.29.0/mql_13.29.0_darwin_arm64.tar.gz"
+    sha256 "4a76dd1078f3e9468dc9edba960f23e7c7680bde2f1920076aa602162ffb0b4e"
+    url "https://releases.mondoo.com/mql/13.29.1/mql_13.29.1_darwin_arm64.tar.gz"
   end
 
   def install
