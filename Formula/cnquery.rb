8b85c5f7d2b25c1fe63b8aedce90c73190393d3e
Mondoo Tools <tools@mondoo.com>
2026-08-19 14:47:04 +0000

    13.35.1

--- Formula/cnquery.rb
+++ Formula/cnquery.rb
@@ -5,15 +5,15 @@
 class Cnquery < Formula
   desc "Transitional package for cnquery to cnspec migration"
   homepage "https://mondoo.com"
-  version "13.35.0"
+  version "13.35.1"
   depends_on "cnspec"
 
   if Hardware::CPU.intel?
-    sha256 "f49ed5c784aa8535058676d7b64e1ca6d2b099dad477ce8bb694e98b93144168"
-    url "https://releases.mondoo.com/cnspec/13.35.0/cnspec_13.35.0_darwin_amd64.tar.gz"
+    sha256 "cb01a7f651f4f6791feff9658615182d986a6f87fd7e397fbc7a434fb713dccd"
+    url "https://releases.mondoo.com/cnspec/13.35.1/cnspec_13.35.1_darwin_amd64.tar.gz"
   else
-    sha256 "bf104f0d9cf58e8e57a1548f6c34a5f83de66bec37ecf9ca29488e1ce2a245c7"
-    url "https://releases.mondoo.com/cnspec/13.35.0/cnspec_13.35.0_darwin_arm64.tar.gz"
+    sha256 "a7724936d78c151b7c22df48eaa55cd2c128746215fbc5ddc5e940e54b03c0e7"
+    url "https://releases.mondoo.com/cnspec/13.35.1/cnspec_13.35.1_darwin_arm64.tar.gz"
   end
 
   def install
